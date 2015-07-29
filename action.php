9eae318d0910f5bd19bb97a8350d4fd3df11311c
amalia.crisan <amalia.crisan@bitstone.eu>
2015-07-29 16:23:15 +0200

    Initial commit

---
+++ action.php
@@ -0,0 +1,18 @@
+
+<html>
+<head>
+
+</head>
+<body>
+    <?php echo $_POST["Country"]; ?>
+    <?php echo $_POST["From"]; ?>
+    <?php echo $_POST["To"]; ?>
+    <?php echo $_POST["Passengers"]; ?>
+    <?php echo $_POST["One"]; ?>
+    <?php echo $_POST["Way"]; ?>
+    <?php echo $_POST["Return"]; ?>
+    <?php echo $_POST["Time"]; ?>
+    <?php echo $_POST["Payment"];?>
+
+</body>
+</html>
