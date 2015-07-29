cdc5344bc85abdfcf3cd593d1023dcb9a8589494
amalia.crisan <amalia.crisan@bitstone.eu>
2015-07-29 16:55:57 +0200

    add php code and fix the hours

--- action.php
+++ action.php
@@ -4,15 +4,34 @@
 
 </head>
 <body>
-    <?php echo $_POST["Country"]; ?>
-    <?php echo $_POST["From"]; ?>
-    <?php echo $_POST["To"]; ?>
-    <?php echo $_POST["Passengers"]; ?>
-    <?php echo $_POST["One"]; ?>
-    <?php echo $_POST["Way"]; ?>
-    <?php echo $_POST["Return"]; ?>
-    <?php echo $_POST["Time"]; ?>
-    <?php echo $_POST["Payment"];?>
 
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $Country = test_input($_POST["Country"]);
+    $From = test_input($_POST["From"]);
+    $To = test_input($_POST["To"]);
+    $Passengers = test_input($_POST["Passengers"]);
+    $One = test_input($_POST["One"]);
+    $Way = test_input($_POST["Way"]);
+    $Return = test_input($_POST["Return"]);
+    $Time = test_input($_POST["Time"]);
+    $Payment = test_input($_POST["Payment"]);
+    }
+
+    function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+    }
+    ?>
+    <h4>Your Input:</h4>
+    Country: <?php echo $_POST["Country"]; ?> <br>
+    From: <?php echo $_POST["From"]; ?> <br>
+    To: <?php echo $_POST["To"]; ?> <br>
+    Passengers: <?php echo $_POST["Passengers"]; ?> <br>
+    One Way: <?php echo $_POST["One"]; ?> <?php echo $_POST["Way"]; ?> <br>
+    Return Time: <?php echo $_POST["Return"]; ?> <?php echo $_POST["Time"]; ?> <br>
+    Payment: <?php echo $_POST["Payment"];?> <br>
 </body>
 </html>
